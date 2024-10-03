db874d7e68330ef3387d96ea810f7bdd8c4abbf9
ejune1 <ejune1@asu.edu>
2024-10-03 14:49:25 +0000

    code complete

--- python/producer_consumer.py
+++ python/producer_consumer.py
@@ -1,9 +1,11 @@
+import time
 from threading import Thread
 from threading import Lock
 from threading import Event
 
 class Messages:
     def __init__(self):
+        #use a list as a queue, pretend it isn't thread safe
         self.__messages = []
         self.__lock = Lock()
         self.__event = Event()
@@ -29,3 +31,54 @@ class Messages:
 
         self.__lock.release()
         return message
+
+class Producer:
+    def __init(self, messages, limit):
+        self.messages = messages
+        self.limit  = limit
+    
+    def produce(self):
+        for x in range(limit):
+            message = random.randint(0, 99)
+            self.messages.send(message)
+            print(f"Producer sent {message}")
+            
+            #pretend some processing is going on
+            sleeptime = random.randint(0,1000)
+            time.sleep(sleeptime / 1000)
+
+        message = -1
+        self.messages.send(message)
+
+        print(f"Producer sent {limit} messages")
+
+class Consumer:
+    def __init(self, messages):
+        self.messages = messages
+
+    def consume(self):
+        message = 0
+        while message != -1:
+            #blocks
+            message = messages.receive()
+            print(f"Consumer got {message}")
+
+            #pretend some processing is going on
+            sleeptime = random.randint(0,1000)
+            time.sleep(sleeptime / 1000)
+
+if __name__ == "__main__":
+    limit = 100
+    messages = Messages()
+
+    producer = Producer(messages, limit)
+    consumer = Consumer(messages)
+
+    producer_thread = Thread(target = producer.produce)
+    consumer_thread = Thread(target = consumer.consume)
+
+    producer.start()
+    consumer.start()
+
+    producer.join()
+    consumer.join()
